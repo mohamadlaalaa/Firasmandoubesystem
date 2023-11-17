9715e19a181f7fa6a4ec20334aa9f6d436d21fa5
mohamadlaalaa <mohamad.laalaa700@gmail.com>
2023-11-17 17:00:44 +0200

    adding phpmailer

--- addorder.php
+++ addorder.php
@@ -11,7 +11,12 @@ $stmt->execute();
 $stmt->bind_result($fullName);
 $stmt->fetch();
 $stmt->close();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 ?>
 
 <?php
@@ -53,9 +58,29 @@ if(isset($_POST["submit"])){
     mysqli_query($con , $query);
     
     
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = "hbaba9377@gmail.com";
+    $mail->Password = 'ckdu aycu vyxd qsyz';
+    $mail->SMTPSecure = 'ssl'; // Change 'ssl' to 'tls'
+    $mail->Port = 465; // Update to the correct port for TLS
+    $mail->setFrom('hbaba9377@gmail.com');
+    $mail->addAddress('mohamad.laalaa700@gmail.com');
+    $mail->isHTML(true);
+    $mail->Subject = 'SP - ' . $orderInvoice; 
+    $mail->Body = $representative . "--"  . $storelocationgovernorate;
     echo "
     <script>alert('Done');window.location.replace('welcome.php');</script>
     ";
+    try {
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+    }
+    
 
   
 
